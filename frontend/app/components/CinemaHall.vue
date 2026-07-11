b4819c766b7a223eea5407c5d3cf00e6d27967a1
birhanu <birhanumatebe12@gmail.com>
2026-07-11 23:51:00 +0300

    feat: add CinemaHall.vue

---
+++ frontend/app/components/CinemaHall.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  hallName: String,
+  rows: { 
+    type: Array, 
+    required: true 
+  }
+})
+</script>
+
+<template>
+  <div class="mt-8 flex flex-col items-center">
+    <span class="text-lime-400">{{ hallName }}</span>
+    
+    <div class="grid grid-cols-4 gap-6 mt-8">
+      <div v-for="seat in rows" :key="seat.id">
+      </div>
+    </div>
+  </div>
+</template>
