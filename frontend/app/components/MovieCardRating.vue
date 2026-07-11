96ec78d20e212aef768124b937bbd9477675422d
birhanu <birhanumatebe12@gmail.com>
2026-07-11 23:52:48 +0300

    feat: MovieCardRating.vue

---
+++ frontend/app/components/MovieCardRating.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  rating: {
+    type: Number,
+    default: 0
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center gap-1">
+    <span 
+      v-for="star in 5" 
+      :key="star"
+      class="text-xl"
+      :class="star <= rating ? 'text-yellow-400' : 'text-gray-600'"
+    >
+      ★
+    </span>
+  </div>
+</template>
